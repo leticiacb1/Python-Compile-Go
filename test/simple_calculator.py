c05b7974d7cedf70785314e321794795c1bd70e3
leticiacb1 <leticiacb1@al.insper.edu.br>
2023-09-14 11:29:01 -0300

    Fixing tests

--- test/simple_calculator.py
+++ test/simple_calculator.py
@@ -5,6 +5,7 @@ Ou rode um test eindividual utilizando:
     > pytest test.py::TestClass::<method>
 '''
 from ..errors.parser import InvalidExpression
+from ..errors.tokens import InvalidToken
 from parser import Parser
 
 class TestSimpleCalculator:
@@ -22,11 +23,24 @@ class TestSimpleCalculator:
         source_code = ' 3 - 2'
         assert Parser().run(source_code) == 1
 
+    def division_with_space(self):
+        '''
+            Should return a correct division value
+        '''
+        source_code = ' 3 / 2'
+        assert Parser().run(source_code) == 1
+
+    def times_with_space(self):
+        '''
+            Should return a correct times value
+        '''
+        source_code = ' 3 * 2'
+        assert Parser().run(source_code) == 6
+
     def without_operator(self): 
         '''
             Should raise a error | Without operator
         '''
-
         with pytest.raises(InvalidExpression) as error: 
             source_code = ' 3  2' 
             Parser().run(source_code)  
@@ -36,7 +50,6 @@ class TestSimpleCalculator:
         '''
             Should raise a error | Invalid Caracter
         '''
-
         with pytest.raises(InvalidExpression) as error: 
             source_code = ' 1 ,+ 1' 
             Parser().run(source_code)  
@@ -51,10 +64,82 @@ class TestSimpleCalculator:
             Parser().run(source_code)  
         assert str(error.value) == f"\n Expected number type | Got {Parser().tokenizer.next} after a operator"
 
-    def correct_value(self): 
+    def sum_sub_correct_value(self): 
         '''
             Should calculate correct value
         '''
-
         source_code = ' 789 +345 - 123'
         assert Parser().run(source_code) == 1011
+
+    def sum_sub_div_times_correct_value(self): 
+        '''
+            Should calculate correct value
+        '''
+        source_code = '2/2 + (4*2+1) -4'
+        assert Parser().run(source_code) == 6
+
+class TestUnaryOperators:
+    def minus_minus(self): 
+        '''
+            Should calculate correct value
+        '''
+        source_code = '- -4'
+        assert Parser().run(source_code) == 4
+
+    def plus_plus(self): 
+        '''
+            Should calculate correct value
+        '''
+        source_code = '+ +4'
+        assert Parser().run(source_code) == 4
+
+    def plus_minus(self): 
+        '''
+            Should calculate correct value
+        '''
+        source_code = '+ -4'
+        assert Parser().run(source_code) == -4
+
+    def calculate_expression(self): 
+        '''
+            Should calculate correct value
+        '''
+        source_code = '+--2/2 + (4*2+1)'
+        assert Parser().run(source_code) == 10
+
+class TestParentheses:
+    def correct_use(self): 
+        '''
+            Should calculate correct value
+        '''
+        source_code = '- (-4+ 2)'
+        assert Parser().run(source_code) == 2
+
+    def incorrect_use(self): 
+        '''
+            Should raise a error
+        '''
+        with pytest.raises(InvalidExpression) as error: 
+            source_code = '- (-4+ 2 '
+            Parser().run(source_code)  
+
+        assert str(error.value) == f"\n Expected close parentheses type | Got {Parser().tokenizer.next}"
+
+class TestComments:
+
+    def ignore_comments_correct_value(self):
+        '''
+            Should calculate correct value
+        '''
+        source_code = '- (-4+ 2) // Testando ignorar +2'
+        assert Parser().run(source_code) == 2
+
+    def ignore_comments_correct_value(self):
+        '''
+            Should raise error
+        '''
+        with pytest.raises(InvalidExpression) as error: 
+            source_code = '- (-4+ 2 // Testando error )'
+            Parser().run(source_code)  
+
+        assert str(error.value) == f"\n Expected close parentheses type | Got {Parser().tokenizer.next}"
